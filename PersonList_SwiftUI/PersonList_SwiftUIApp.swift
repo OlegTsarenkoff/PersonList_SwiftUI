74f458844a54b7bab455fe4266a98fc887b2b24f
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-10 14:44:40 +0300

    Initial Commit

---
+++ PersonList_SwiftUI/PersonList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonList_SwiftUIApp.swift
+//  PersonList_SwiftUI
+//
+//  Created by Oleg Tsarenkoff on 10.08.21.
+//
+
+import SwiftUI
+
+@main
+struct PersonList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
